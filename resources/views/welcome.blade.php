fb28deff16008a4312c15486c9322bf5c040092f
Thiago Magano <thiagomagano1993@outlook.com>
2025-09-25 12:18:19 -0300

    CI/CD CONFI TEST

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
     <div
         class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
         <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-            <h1 class="text-white font-bold text-xl">Hello NOC FUTSAL! </h1>
+            <h1 class="text-white font-bold text-xl">Hello NOC FUTSAL WITH CI/CD! </h1>
         </main>
 
         @if (Route::has('login'))
