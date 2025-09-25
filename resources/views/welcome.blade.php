ee70981aacaf4f8636fb65632b78e7ab42d1040c
Thiago Magano <thiagomagano1993@outlook.com>
2025-09-25 15:34:37 -0300

    exclude CI/CD for now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,6 @@
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-
-
         </style>
     @endif
 </head>
